d2796d30f419deddac126ebeaaa0a4fa6fc11550
Stellaric <hello@stellaric.pw>
2022-03-11 23:22:59 -0600

    load images from folder onto webpage. fresh start

---
+++ stlrcx/routes.py
@@ -0,0 +1,14 @@
+import os
+from flask import render_template, url_for, flash, redirect, request, send_from_directory
+from stlrcx import app  #, db, bcrypt
+
+@app.route("/")
+def home():
+    images = os.listdir(imageDir)
+    return render_template('home.jinja', images=images, imageDir=imageDir)
+
+imageDir = "/Users/stellaric/Downloads/testing/"
+
+@app.route('/uploads/<path:filename>')
+def download_file(filename):
+    return send_from_directory(imageDir, filename, as_attachment=True)
