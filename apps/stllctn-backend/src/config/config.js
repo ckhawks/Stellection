51a0bf115b32d1b416514c2bf87233b869d17fc6
Carter Hawks <carterhawks@agile-mblt-042.local>
2023-01-28 03:26:21 -0600

    Updated prod deployment settings, alternate docker compose. new start cmd is npm run up:build

--- apps/stllctn-backend/src/config/config.js
+++ apps/stllctn-backend/src/config/config.js
@@ -17,10 +17,17 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.PG_USERNAME,
+    "password": process.env.PG_PASSWORD,
+    "database": process.env.PG_DATABASE,
+    "port": process.env.PG_PORT,
+    "host": process.env.PG_HOST,
     "dialect": "postgres"
+    // "username": "root",
+    // "password": null,
+    // "database": "database_production",
+    // "host": "127.0.0.1",
+    // "dialect": "postgres",
+    // "port":
   }
 }
