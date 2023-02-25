12155cbb17f7b6b68b49283c038dbe82be9e7c7f
ckhawks <carter.hawks@agilevelocity.com>
2023-02-25 04:08:53 -0600

    pagination for get all and next/prev for get one

--- apps/stllctn-backend/src/controllers/star.controller.js
+++ apps/stllctn-backend/src/controllers/star.controller.js
@@ -31,7 +31,7 @@ exports.create = (req, res) => {
 };
 
 // Retrieve all stars from the database
-exports.findAll = (req, res) => {
+exports.findAll2 = (req, res) => {
   // If there's a name provided in the query, use it as the search condition
   const name = req.query.name;
   var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
@@ -59,11 +59,61 @@ exports.findAll = (req, res) => {
     });
 };
 
+// pagination from https://stackoverflow.com/questions/61791627/get-next-offset-for-pagination-sequelize
+exports.findAll = async (req, res) => {
+  const page = parseInt(req.query.page) || 0;
+  const limit = parseInt(req.query.limit) || 10;
+
+  try {
+    const result = await Star.findAndCountAll({
+      // where: {
+      //   active: "1",
+      // },
+      offset: limit * page,
+      limit,
+      order: [["star_id", "ASC"]],
+    });
+
+    res.json({
+      page,
+      limit,
+      total: result.count,
+      data: result.rows,
+    });
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+};
+
+// lists all products
+// GET /products
+// GET /products?limit=2&page=2
+// ============================================================
+const listAllProducts = async (req, res) => {
+  const page = req.query.page | 0;
+  const limit = req.query.limit | 10;
+
+  try {
+    const result = await db.products.findAndCountAll({
+      where: {
+        active: "1",
+      },
+      offset: limit * page,
+      limit,
+      order: [["id", "ASC"]],
+    });
+
+    res.json(util.response.paging(result, page, limit));
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+};
+
 // Retrieve a single star by id
-exports.findOne = (req, res) => {
+exports.findOne = async (req, res) => {
   const id = req.params.starId;
 
-  Star.findByPk(id, {
+  await Star.findByPk(id, {
     include: [
       {
         model: db.Cluster,
@@ -72,10 +122,55 @@ exports.findOne = (req, res) => {
         attributes: ["cluster_id", "cluster_name", "public"],
       },
     ],
+    plain: true,
   })
-    .then((data) => {
+    .then(async (data) => {
       if (data) {
-        res.send(data);
+        const next = await Star.findAll({
+          attributes: ["star_id"],
+          where: {
+            star_id: {
+              [Op.gt]: data.star_id,
+            },
+          },
+          limit: 1,
+          plain: true,
+        });
+
+        const prev = await Star.findAll({
+          attributes: ["star_id"],
+          where: {
+            star_id: {
+              [Op.lt]: data.star_id,
+            },
+          },
+          limit: 1,
+          plain: true,
+        });
+
+        // plain: true because https://stackoverflow.com/a/71090541
+        const data_plain = data.get({ plain: true });
+        console.log("data_plain: ", data_plain);
+        // console.log("prev_plain", prev_plain);
+
+        // console.log("prev", prev.star_id);
+        console.log("next", next);
+        // console.log("star", data);
+        // console.log("star/clusters", data.clusters);
+
+        data = { next: next, prev: prev, ...data_plain };
+        console.log("data", data);
+
+        // const sendThis = {
+        //   data: {
+        //     next: next.star_id,
+        //     prev: prev.star_id,
+        //     ...data,
+        //   },
+        // };
+
+        // console.log(sendThis);
+        res.send({ data: data });
       } else {
         res.status(404).send({
           message: `Cannot find Star with id: ${id}`,
@@ -83,6 +178,7 @@ exports.findOne = (req, res) => {
       }
     })
     .catch((err) => {
+      console.log(err);
       res.status(500).send({
         message: `Error retrieving Star with id: ${id}`,
       });
